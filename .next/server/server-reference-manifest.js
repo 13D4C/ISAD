50c9995c04c73b5b9b533322d3b97664075a1c68
NettaeKrub <Undaaa12345@gmail.com>
2024-07-28 01:22:21 +0700

    alo

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZUqRs9nzi0cQUEbHs3jDglvEZuBJZgmTjfejbdVRCOU=\"\n}"
---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['cmms.kmitl.ac.th'],
+    },
+}
+
+export default nextConfig;
