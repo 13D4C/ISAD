0fd0d9cf133ecd933fc8babba3cb7722163cd7ea
NettaeKrub <Undaaa12345@gmail.com>
2024-09-09 22:40:46 +0700

    fix api path

--- Example/src/app/page.tsx
+++ Example/src/app/page.tsx
@@ -80,11 +80,13 @@ const LoginPage: React.FC = () => {
     try {
       if (email.slice(-12) == "@kmitl.ac.th")
       {
-        const response = await axios.post('http://localhost:8888/login', { email, password });
+        const response = await axios.post('http://localhost:8888/api/login', { email, password });
         if (response.status === 200) {
           localStorage.setItem('token', response.data.token);
           router.push('/lima'); // Redirect to a protected route or dashboard
         }
+      } else {
+        console.error("Error");
       }
     } catch (error) {
       console.error('Error logging in', error);
