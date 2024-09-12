73f0314189a54edc4d9c46ef735131dd3ce4ce56
Sirikul Khummung <135794016+Imparaloy@users.noreply.github.com>
2024-09-12 10:28:20 +0700

    Update Nabbar Add bluebox ontop

--- Frontend/Test/layout.tsx
+++ Frontend/Test/layout.tsx
@@ -19,7 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        {/* Blue Bar */}
+        <div className="bg-blue-700 h-10 w-full fixed top-0 left-0 z-10"></div>
+        {/* End Blue Bar */}
+
+        {/* Navigation Bar */}
+        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-2 start-0 border-b border-gray-200 dark:border-gray-600">
           {/* Box NavBar */}
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             {/* logo IT kmitl*/}
@@ -72,18 +77,20 @@ export default function RootLayout({
             </div>
           </div>
         </nav>
-        {/* End Box NavBar */}
+        {/* End Navigation Bar */}
+        
         {children}
-        {/* Footer of page*/}
+
+        {/* Footer of page */}
         <footer className="bg-white rounded-lg shadow dark:bg-gray-900">
           <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            &copy; 2025 IT next. School of Information Technology KMITL.
+              &copy; 2025 IT next. School of Information Technology KMITL.
             </span>
           </div>
         </footer>
-        {/* End Footer of page*/}
+        {/* End Footer of page */}
       </body>
     </html>
   );
