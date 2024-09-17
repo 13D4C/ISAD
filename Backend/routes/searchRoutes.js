1284cef479fc668943a66e8aa31b913b5fa0cddd
NettaeKrub <Undaaa12345@gmail.com>
2024-09-17 18:45:42 +0700

    setUp OOP

---
+++ Backend/routes/searchRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const SearchController = require('../Controller/searchController');
+
+router.get('/search', (req, res) => SearchController.search(req, res));
+
+module.exports = router;
+
+
