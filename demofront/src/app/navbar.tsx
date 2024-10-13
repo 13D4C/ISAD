2194a3b9b79c5217424be89e5aa2fb605b4003ff
NettaeKrub <Undaaa12345@gmail.com>
2024-10-13 21:58:09 +0700

    update navbar

--- demofront/src/app/navbar.tsx
+++ demofront/src/app/navbar.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import axios from "axios";
+import Image from 'next/image';
 
 export default function Navbar() {
     const router = useRouter();
@@ -39,7 +40,7 @@ export default function Navbar() {
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 {/* logo IT kmitl*/}
                 <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-
+                    <Image src="/itlogo.png" alt="IT KMITL Logo" width={48} height={48} /> {/* Set width and height */}
                 </Link>
                 {/* Middle of Nav Bar */}
                 <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
